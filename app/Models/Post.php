b06defa01d4037e57e0bf5ac8e72a1aaedd07a4c
Snpyyy <snpyyy.a@gmail.com>
2022-07-01 18:03:01 +0900

    投稿一覧画面デザイン作成

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function getFormattedDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['updated_at'])->format('Y/m/d H:i');
+    }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,18 +4,14 @@
 
 @section('content')
 @foreach($posts as $post)
-    <div>
-        <div>
-            <p>投稿者: {{ $post->name }}</p>
-            <p>投稿日時: {{ $post->updated_at }}</p>
+    <div class="w-50 bg-white mx-auto mb-5 p-2 shadow-sm">
+        <div class="d-flex">
+            <p class="me-3">投稿者: <span style="font-weight: bold">{{ $post->name }}</span></p>
+            <p>投稿日時: {{ $post->formatted_date }}</p>
         </div>
         <div>
-            <p>本文</p>
-            <p>{{ $post->body }}</p>
+            <p class="p-1 bg-light">{!! nl2br(e($post->body)) !!}</p>
         </div>
     </div>
 @endforeach
-<div>
-    <a href="/create">投稿する</a>
-</div>
 @endsection

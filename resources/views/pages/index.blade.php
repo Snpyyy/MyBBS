6beaf481e88c14235a1ef2e61e6836f461b697f7
Snpyyy <snpyyy.a@gmail.com>
2022-07-02 13:20:05 +0900

    add ページング

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,15 +3,29 @@
 @section('title', '一覧表示画面')
 
 @section('content')
-@foreach($posts as $post)
-    <div class="w-50 bg-white mx-auto mb-5 p-2 shadow-sm">
-        <div class="d-flex">
-            <p class="me-3">投稿者: <span style="font-weight: bold">{{ $post->name }}</span></p>
-            <p>投稿日時: {{ $post->formatted_date }}</p>
+<div class="container">
+    @foreach($posts as $post)
+        <div class="w-50 bg-white mx-auto mb-5 p-2 shadow-sm">
+            <div class="d-flex">
+                <p class="me-3">投稿者: <span style="font-weight: bold">{{ $post->name }}</span></p>
+                <p>投稿日時: {{ $post->formatted_date }}</p>
+            </div>
+            <div>
+                <p class="p-1 bg-light">{!! nl2br(e($post->body)) !!}</p>
+            </div>
         </div>
-        <div>
-            <p class="p-1 bg-light">{!! nl2br(e($post->body)) !!}</p>
+    @endforeach
+    @if ($posts->isEmpty())
+        <div class="w-50 mx-auto bg-white shadow-sm p-5 mt-5">
+            <h2 class="text-center mb-5">まだ投稿がありません</h2>
+            <div class="d-grid gap-2">
+                <a href="/create" class="btn btn-primary">投稿する</a>
+            </div>
         </div>
+    @endif
+    <div class="d-flex justify-content-center">
+        {{ $posts->links() }}
     </div>
-@endforeach
+</div>
+
 @endsection
